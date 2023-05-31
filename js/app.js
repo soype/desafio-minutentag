309dd9d8e36e43fdfaa08b8a9b5ca5283fac3b89
soype <phenestroza@gmail.com>
2023-05-30 21:21:17 -0300

    Finnished mobile

--- js/app.js
+++ js/app.js
@@ -63,10 +63,13 @@ const extractId = (path) => {
 
 const expandText = () => {
   let p = document.querySelector(".desc-info");
+  let readMore = document.querySelector(".read-more");
   if (p.classList.contains("expanded")) {
     p.classList = "desc-info";
+    readMore.textContent = "Read more";
   } else {
     p.classList.add("expanded");
+    readMore.textContent = "Read less";
   }
 };
 
@@ -116,6 +119,7 @@ const renderSingle = (id) => {
       const readMore = document.createElement("p");
       readMore.classList = "read-more";
       readMore.textContent = "Read more";
+      readMore.onclick = expandText;
       // Bring the elements together
       desc.appendChild(descTitle);
       desc.appendChild(info);
@@ -149,7 +153,14 @@ const renderSingle = (id) => {
       cartContainer.classList = "cart-container";
       const cartButton = document.createElement("div");
       cartButton.classList = "deatil-cart-button";
-      cartButton.textContent = `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="12" cy="6" r="4.25" stroke="#FF9F24" stroke-width="1.5"/><path d="M4.30623 9.59689C4.50953 7.97049 5.89208 6.75 7.53113 6.75H16.4689C18.1079 6.75 19.4905 7.97049 19.6938 9.59689L20.6938 17.5969C20.9362 19.5367 19.4237 21.25 17.4689 21.25H6.53113C4.57626 21.25 3.06375 19.5367 3.30623 17.5969L4.30623 9.59689Z" fill="white" stroke="#FF9F24" stroke-width="1.5"/><circle cx="9.75" cy="10.75" r="0.75" fill="#FF9F24"/><circle cx="13.75" cy="10.75" r="0.75" fill="#FF9F24"/></svg>`;
+      cartButton.innerHTML = `<svg width="54" height="54" viewBox="0 0 54 54" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect x="0.4" y="0.4" width="53.2" height="53.2" rx="11.6" stroke="#FF9F24" stroke-width="0.8"/>
+      <circle cx="27" cy="21" r="4.25" stroke="#FF9F24" stroke-width="1.5"/>
+      <path d="M19.3062 24.5969C19.5095 22.9705 20.8921 21.75 22.5311 21.75H31.4689C33.1079 21.75 34.4905 22.9705 34.6938 24.5969L35.6938 32.5969C35.9362 34.5367 34.4237 36.25 32.4689 36.25H21.5311C19.5763 36.25 18.0638 34.5367 18.3062 32.5969L19.3062 24.5969Z" fill="white" stroke="#FF9F24" stroke-width="1.5"/>
+      <circle cx="24.75" cy="25.75" r="0.75" fill="#FF9F24"/>
+      <circle cx="28.75" cy="25.75" r="0.75" fill="#FF9F24"/>
+      </svg>
+      `;
       const addToCart = document.createElement("button");
       addToCart.classList = "detail-add-button";
       addToCart.textContent = "Add to cart";
@@ -162,7 +173,7 @@ const renderSingle = (id) => {
       container.appendChild(subData);
       container.appendChild(desc);
       container.appendChild(sizeContainer);
-      container.appendChild(addToCart);
+      container.appendChild(cartContainer);
 
       /// Logic for selecting size option
       let detailOptions = document.querySelectorAll(".option");
